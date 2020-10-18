18a185dde0fdaef596209cfd2b2e613a35f79bd0
Emma Valen Rian <emmavr@stud.ntnu.no>
2020-10-18 19:25:39 +0200

    #4 Toggle button for filters on small screens

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import {Container, Row, Col } from "react-bootstrap";
+import React, {useState} from "react";
+import {useParams} from "react-router-dom";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import Sidebar from "../components/filter/Sidebar";
 
 import "./Results.css"
@@ -10,20 +10,37 @@ const Results = () => {
     //her kan vi ha search component for å initialisere nytt søk, og også inkludere komponent for
     //filtrering og sortering
     const { searchText } = useParams()
+
+    const [showFilters, setShowFilters] = useState(false)
+
+    const toggleFilters = () => {
+        setShowFilters(!showFilters)
+    }
+
+    const show = showFilters ? "show" : "" ;
+
     return (
         <div className="results-wrapper">
             <Container fluid>
                 <Row>
-                    <Col sm={4} className="sidebar-wrapper">
-                        <Sidebar />
+                    <Col sm={4} className={"d-block d-sm-none"}>
+                        <Button className="btn-group-toggle" type="button" variant={"primary"} onClick={ toggleFilters }>
+                            Filtrer
+                        </Button>
+                        <div className={"collapse" + show}>
+                            <Sidebar/>
+                        </div>
+                    </Col>
+                    <Col sm={4} className={"d-none d-sm-block"}>
+                        <Sidebar/>
                     </Col>
                     <Col  sm={8} className="page-content-wrapper">
-                        {/*Legg inn tabell som viser resultat fra søk under her*/}
                         <h5>Dette er resultatene fra søket: {searchText}</h5>
                     </Col>
                 </Row>
-
             </Container>
+
+
         </div>
     );
 }
