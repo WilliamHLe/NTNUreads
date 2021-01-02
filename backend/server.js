5d598db04a24d548b9faf96b842834f771a6fd8c
WilliamHLe <le.william.h@outlook.com>
2021-01-02 18:59:24 +0100

    Deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use('/review', reviewRoute)
 
 
 // PORT
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
 })
