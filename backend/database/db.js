f305462a2c0a2b0f1688ddf0c382e18e198159b9
WilliamHLe <le.william.h@outlook.com>
2021-01-02 18:31:25 +0100

    Deploy to heroku

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://siverhog:siverhog2810gruppe13@it2810-13.idi.ntnu.no:27017/it281013p3'
+    db: 'mongodb+srv://williahl:wilymongo23@cluster0.ok9no.mongodb.net/test?authSource=admin&replicaSet=atlas-e37pef-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 };
